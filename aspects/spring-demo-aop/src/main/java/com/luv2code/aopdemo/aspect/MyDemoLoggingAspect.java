1d1860ca7ed3596a049001ada844e9719546406c
Eric Balawejder <eric.balawejder@gmail.com>
2020-08-31 14:57:59 -0400

    match given method in a specific class

--- aspects/spring-demo-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aspects/spring-demo-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,7 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
