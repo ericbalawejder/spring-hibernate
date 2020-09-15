8948b4105f8d1ca74590ee85126997adcef38e21
Eric Balawejder <eric.balawejder@gmail.com>
2020-09-15 13:46:13 -0400

    creating pointcut declarations

---
+++ aspects/spring-demo-aop-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,17 @@
+package com.luv2code.aopdemo.dao;
+
+import com.luv2code.aopdemo.Account;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount(Account account, boolean vipFlag) {
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+    }
+
+    public boolean doWork() {
+        System.out.println(getClass() + ": doWork()");
+        return false;
+    }
+}
