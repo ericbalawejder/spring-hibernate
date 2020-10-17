de95da38ff288407a68959a5a945584ead0d535c
Eric Balawejder <eric.balawejder@gmail.com>
2020-10-16 20:56:50 -0400

    Fixed broken urlPattern

--- webdemo/src/main/java/org/gradle/demo/HelloServlet.java
+++ webdemo/src/main/java/org/gradle/demo/HelloServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1)
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"}, loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
