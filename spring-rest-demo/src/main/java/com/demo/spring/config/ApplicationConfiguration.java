6a0f41dc88e4ced838e8feb2ccffbfe5e37d4c55
Eric Balawejder <eric.balawejder@gmail.com>
2020-11-18 17:15:52 -0500

    spring rest demo setup

---
+++ spring-rest-demo/src/main/java/com/demo/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package com.demo.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.demo.spring")
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+}
+
---
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ericbalawejder
+  Date: 11/18/20
+  Time: 3:27 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Spring Rest Demo</title>
+  </head>
+  <body>
+  Spring Rest Demo
+  <br>
+  <br>
+  <a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+  </body>
+</html>
