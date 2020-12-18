7d2f81184a89a9be7ccf9ed635f6d80919b67220
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-18 17:05:44 -0500

    Added @Pathvariable for studentId and exception handling for bad studentId input

--- spring-rest-demo/src/main/java/com/demo/spring/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/demo/spring/rest/StudentRestController.java
@@ -1,9 +1,15 @@
 package com.demo.spring.rest;
 
 import com.demo.spring.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +18,26 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
-
-        return students;
     }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return List.copyOf(students);
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        if ((studentId >= students.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return students.get(studentId);
+    }
 }
