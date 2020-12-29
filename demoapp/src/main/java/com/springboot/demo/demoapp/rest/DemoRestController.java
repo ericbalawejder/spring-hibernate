05fad40d875e7350b24fa6fe61ed7130f60ebc4a
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-28 19:22:55 -0500

    Created controller and added spring boot actuator

---
+++ demoapp/src/main/java/com/springboot/demo/demoapp/rest/DemoRestController.java
@@ -0,0 +1,21 @@
+package com.springboot.demo.demoapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class DemoRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is " + LocalDateTime.now();
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorlout() {
+        return "Run a hard 5K";
+    }
+
+}
