23abd0b50ff9f7e3dbfb31d23d2815e69079077a
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-29 15:41:21 -0500

    How to run from command line

--- demoapp/src/main/java/com/springboot/demo/demoapp/DemoappApplication.java
+++ demoapp/src/main/java/com/springboot/demo/demoapp/DemoappApplication.java
@@ -3,6 +3,13 @@ package com.springboot.demo.demoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Command line run:
+// $ gradle build
+// $ java -jar ./build/libs/demoapp-0.0.1-SNAPSHOT.war
+
+// Gradle Spring Boot plugin:
+// $ gradle bootRun
+
 @SpringBootApplication
 public class DemoappApplication {
 
